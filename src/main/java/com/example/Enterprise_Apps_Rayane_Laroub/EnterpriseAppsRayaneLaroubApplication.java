046467b283f4c7b0b95f9c5c8b25053d0a6caada
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-06-11 20:41:04 +0200

    initial commit

---
+++ src/main/java/com/example/Enterprise_Apps_Rayane_Laroub/EnterpriseAppsRayaneLaroubApplication.java
@@ -0,0 +1,13 @@
+package com.example.Enterprise_Apps_Rayane_Laroub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppsRayaneLaroubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAppsRayaneLaroubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Enterprise_Apps_Rayane_Laroub/EnterpriseAppsRayaneLaroubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Enterprise_Apps_Rayane_Laroub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAppsRayaneLaroubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
