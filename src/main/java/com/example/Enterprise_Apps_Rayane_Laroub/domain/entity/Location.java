8663fc10aab57b5c84772da694ea82b6c605292f
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-06-11 21:14:19 +0200

    FEAT: Location entity

---
+++ src/main/java/com/example/Enterprise_Apps_Rayane_Laroub/domain/entity/Location.java
@@ -0,0 +1,89 @@
+package com.example.Enterprise_Apps_Rayane_Laroub.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "locations")
+public class Location {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "capacity", nullable = false)
+    private int capacity;
+
+    public Location() {
+    }
+
+    public Location(String name, String address, int capacity) {
+        this.name = name;
+        this.address = address;
+        this.capacity = capacity;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return capacity == location.capacity && Objects.equals(id, location.id) && Objects.equals(name, location.name) && Objects.equals(address, location.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, capacity);
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
